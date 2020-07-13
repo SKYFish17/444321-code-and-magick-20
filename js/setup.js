944b55f2be7a129e6469c0e48df01f4e28cc5338
SKYFish17 <nwcorp93@yandex.ru>
2020-07-13 16:08:11 +0300

    создаёт модуль управления окна setup, рефакторит код модуля setup

--- js/setup.js
+++ js/setup.js
@@ -4,13 +4,6 @@
   var setupWindow = document.querySelector('.setup');
   var setupSimilar = setupWindow.querySelector('.setup-similar');
 
-  window.data.createWizardsList();
-  setupSimilar.classList.remove('hidden');
-
-  var setupOpenBtn = document.querySelector('.setup-open');
-  var setupCloseBtn = setupWindow.querySelector('.setup-close');
-  var setupUserName = setupWindow.querySelector('.setup-user-name');
-
   var wizardCoat = setupWindow.querySelector('.wizard-coat');
   var inputCoatColor = setupWindow.querySelector('input[name="coat-color"]');
 
@@ -19,12 +12,6 @@
 
   var fireball = setupWindow.querySelector('.setup-fireball-wrap');
   var inputFireballColor = setupWindow.querySelector('input[name="fireball-color"]');
-
-  var onSetupEscPress = function (evt) {
-    if (evt.code === 'Escape' && evt.target !== setupUserName) {
-      closeSetup();
-    }
-  };
 
   var onWizardCoatClick = function () {
     window.colorize(wizardCoat, inputCoatColor, window.data.wizardsCoatsColors);
@@ -38,39 +25,10 @@
     window.colorize(fireball, inputFireballColor, window.data.wizardFireballColors);
   };
 
-  var openSetup = function () {
-    setupWindow.classList.remove('hidden');
-    document.addEventListener('keydown', onSetupEscPress);
-    wizardCoat.addEventListener('click', onWizardCoatClick);
-    wizardEyes.addEventListener('click', onWizardEyesClick);
-    fireball.addEventListener('click', onFireballClick);
-  };
+  window.data.createWizardsList();
+  setupSimilar.classList.remove('hidden');
 
-  var closeSetup = function () {
-    setupWindow.classList.add('hidden');
-    document.removeEventListener('keydown', onSetupEscPress);
-    wizardCoat.removeEventListener('click', onWizardCoatClick);
-    wizardEyes.removeEventListener('click', onWizardEyesClick);
-    fireball.removeEventListener('click', onFireballClick);
-  };
-
-  setupOpenBtn.addEventListener('click', function () {
-    openSetup();
-  });
-
-  setupOpenBtn.addEventListener('keydown', function (evt) {
-    if (evt.code === 'Enter') {
-      openSetup();
-    }
-  });
-
-  setupCloseBtn.addEventListener('click', function () {
-    closeSetup();
-  });
-
-  setupCloseBtn.addEventListener('keydown', function (evt) {
-    if (evt.code === 'Enter') {
-      closeSetup();
-    }
-  });
+  wizardCoat.addEventListener('click', onWizardCoatClick);
+  wizardEyes.addEventListener('click', onWizardEyesClick);
+  fireball.addEventListener('click', onFireballClick);
 })();
