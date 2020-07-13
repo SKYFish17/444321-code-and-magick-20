23db5afde7c7003574a20ab35a0c439a985e6ca1
SKYFish17 <nwcorp93@yandex.ru>
2020-07-13 14:11:27 +0300

    перенос ф-й генерации и отрисовки списка похожих персонажей в модуль data

--- js/setup.js
+++ js/setup.js
@@ -1,116 +1,76 @@
 'use strict';
 
-var setupWindow = document.querySelector('.setup');
-var setupSimilar = setupWindow.querySelector('.setup-similar');
-var setupSimilarList = setupSimilar.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('div');
+(function () {
+  var setupWindow = document.querySelector('.setup');
+  var setupSimilar = setupWindow.querySelector('.setup-similar');
 
-//  создаёт нового персонажа
-var createWizardItem = function (index) {
-  var newWizard = similarWizardTemplate.cloneNode(true);
-  var newWizardName = newWizard.querySelector('.setup-similar-label');
-  var newWizardAppearance = newWizard.querySelector('.setup-similar-wizard');
-  var newWizardCoat = newWizardAppearance.querySelector('.wizard-coat');
-  var newWizardEyes = newWizardAppearance.querySelector('.wizard-eyes');
+  window.data.createWizardsList();
+  setupSimilar.classList.remove('hidden');
 
-  newWizardName.textContent = window.data.wizardsData[index].name;
-  newWizardCoat.style.fill = window.data.wizardsData[index].coatColor;
-  newWizardEyes.style.fill = window.data.wizardsData[index].eyesColor;
+  var setupOpenBtn = document.querySelector('.setup-open');
+  var setupCloseBtn = setupWindow.querySelector('.setup-close');
+  var setupUserName = setupWindow.querySelector('.setup-user-name');
 
-  return newWizard;
-};
+  var wizardCoat = setupWindow.querySelector('.wizard-coat');
+  var inputCoatColor = setupWindow.querySelector('input[name="coat-color"]');
 
-//  создаёт список похожих персонажей и вставляет в разметку
-var createWizardsList = function () {
-  var fragment = document.createDocumentFragment();
+  var wizardEyes = setupWindow.querySelector('.wizard-eyes');
+  var inputEyesColor = setupWindow.querySelector('input[name="eyes-color"]');
 
-  for (var i = 0; i < window.data.wizardsData.length; i++) {
-    fragment.appendChild(createWizardItem(i));
-  }
-  setupSimilarList.appendChild(fragment);
-};
+  var fireball = setupWindow.querySelector('.setup-fireball-wrap');
+  var inputFireballColor = setupWindow.querySelector('input[name="fireball-color"]');
 
-window.data.generateWizardsData();
-createWizardsList();
-setupSimilar.classList.remove('hidden');
+  var onSetupEscPress = function (evt) {
+    if (evt.code === 'Escape' && evt.target !== setupUserName) {
+      closeSetup();
+    }
+  };
 
-var setupOpenBtn = document.querySelector('.setup-open');
-var setupCloseBtn = setupWindow.querySelector('.setup-close');
-var setupUserName = setupWindow.querySelector('.setup-user-name');
+  var onWizardCoatClick = function () {
+    window.colorize(wizardCoat, inputCoatColor, window.data.wizardsCoatsColors);
+  };
 
-var wizardCoat = setupWindow.querySelector('.wizard-coat');
-var inputCoatColor = setupWindow.querySelector('input[name="coat-color"]');
+  var onWizardEyesClick = function () {
+    window.colorize(wizardEyes, inputEyesColor, window.data.wizardsEyesColors);
+  };
 
-var wizardEyes = setupWindow.querySelector('.wizard-eyes');
-var inputEyesColor = setupWindow.querySelector('input[name="eyes-color"]');
+  var onFireballClick = function () {
+    window.colorize(fireball, inputFireballColor, window.data.wizardFireballColors);
+  };
 
-var fireball = setupWindow.querySelector('.setup-fireball-wrap');
-var inputFireballColor = setupWindow.querySelector('input[name="fireball-color"]');
+  var openSetup = function () {
+    setupWindow.classList.remove('hidden');
+    document.addEventListener('keydown', onSetupEscPress);
+    wizardCoat.addEventListener('click', onWizardCoatClick);
+    wizardEyes.addEventListener('click', onWizardEyesClick);
+    fireball.addEventListener('click', onFireballClick);
+  };
 
-var onSetupEscPress = function (evt) {
-  if (evt.code === 'Escape' && evt.target !== setupUserName) {
-    closeSetup();
-  }
-};
+  var closeSetup = function () {
+    setupWindow.classList.add('hidden');
+    document.removeEventListener('keydown', onSetupEscPress);
+    wizardCoat.removeEventListener('click', onWizardCoatClick);
+    wizardEyes.removeEventListener('click', onWizardEyesClick);
+    fireball.removeEventListener('click', onFireballClick);
+  };
 
-var changeWizardElementColor = function (element, input, elementColors) {
-  var randomColor = window.util.getRandomValue(elementColors);
-
-  switch (element.tagName) {
-    case 'use':
-      element.style.fill = randomColor;
-      break;
-    case 'DIV':
-      element.style.backgroundColor = randomColor;
-      break;
-  }
-  input.value = randomColor;
-};
-
-var onWizardCoatClick = function () {
-  changeWizardElementColor(wizardCoat, inputCoatColor, window.data.wizardsCoatsColors);
-};
-
-var onWizardEyesClick = function () {
-  changeWizardElementColor(wizardEyes, inputEyesColor, window.data.wizardsEyesColors);
-};
-
-var onFireballClick = function () {
-  changeWizardElementColor(fireball, inputFireballColor, window.data.wizardFireballColors);
-};
-
-var openSetup = function () {
-  setupWindow.classList.remove('hidden');
-  document.addEventListener('keydown', onSetupEscPress);
-  wizardCoat.addEventListener('click', onWizardCoatClick);
-  wizardEyes.addEventListener('click', onWizardEyesClick);
-  fireball.addEventListener('click', onFireballClick);
-};
-
-var closeSetup = function () {
-  setupWindow.classList.add('hidden');
-  document.removeEventListener('keydown', onSetupEscPress);
-  wizardCoat.removeEventListener('click', onWizardCoatClick);
-  wizardEyes.removeEventListener('click', onWizardEyesClick);
-  fireball.removeEventListener('click', onFireballClick);
-};
-
-setupOpenBtn.addEventListener('click', function () {
-  openSetup();
-});
-
-setupOpenBtn.addEventListener('keydown', function (evt) {
-  if (evt.code === 'Enter') {
+  setupOpenBtn.addEventListener('click', function () {
     openSetup();
-  }
-});
+  });
 
-setupCloseBtn.addEventListener('click', function () {
-  closeSetup();
-});
+  setupOpenBtn.addEventListener('keydown', function (evt) {
+    if (evt.code === 'Enter') {
+      openSetup();
+    }
+  });
 
-setupCloseBtn.addEventListener('keydown', function (evt) {
-  if (evt.code === 'Enter') {
+  setupCloseBtn.addEventListener('click', function () {
     closeSetup();
-  }
-});
+  });
+
+  setupCloseBtn.addEventListener('keydown', function (evt) {
+    if (evt.code === 'Enter') {
+      closeSetup();
+    }
+  });
+})();
